3f79a24fa2961d034d20b713a0d91a97569722a5
maroluke <visitme@lukacmarko.com>
2018-08-29 09:21:34 +0200

    add landingpage

---
+++ ip-telefonie.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Anmeldung zum Newsletter</title>
+		<!-- <script type="text/javascript">function onSubmit() { document.onlineForm.submit(); }</script> -->
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body class="lp">
+		<!--[if lte IE 9]>
+		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+		<![endif]-->
+
+		<section class="section start">
+			<section class="inner">
+				<span class="title-topic">IP-Telefonie</span>
+				<h1>Der Wechsel in die Zukunft</h1>
+
+				<img src="img/header.png" alt="Der Wechsel in die Zukunft" title="Der Wechsel in die Zukunft">
+
+				<p class="intro">Die Schweiz stellt aktuell die alte Festnetztelefonie vollständig auf All IP um. Das bedeutet: Künftig nutzt das Telefonsignal die gleiche Technik wie das Internet; das Internet Protokoll – kurz IP.</p>
+
+				<p>All IP ist der neue, weltweit bewährte Standard und Basis für unsere Kommunikation. Die veraltete Festnetztelefonie konnte mit den Anforderungen der Zeit nicht mehr mithalten. Die Digitalisierung der Kommunikation ist daher die logische Konsequenz.</p>
+			</section>
+		</section>
+
+		<section class="section grey-bg">
+			<section class="inner topic-item">
+				<div class="topic-name"><span>Fokus</span></div>
+				<span class="title-topic padding-top">Gut zu wissen</span>
+				<h1>Was bringt Ihnen All IP?</h1>
+
+				<p>Durch die Umstellung der Telefonie auf das Internet Protokoll stehen Ihnen zahlreiche hilfreiche Funktionen zur Verfügung. Alle mit dem Ziel, den Arbeitsalltag zu erleichtern. Nutzen Sie die neuen Freiheiten und Chancen, die Ihnen IP ermöglicht.</p>
+			</section>
+
+			<section class="inner topic-item">
+				<img src="img/image-01.png" alt="Immer und überall mobil." title="Immer und überall mobil.">
+
+				<h2>Immer und überall mobil.</h2>
+
+				<p>Mit IP-Telefonie sind Sie weder an Standort, Gerät oder Anwendung gebunden. Sie arbeiten von überall aus produktiv; sei es unterwegs, im Homeoffice oder im Ausland. Ihre herkömmliche Festnetznummer lässt sich auf jedes Gerät übertragen, das für Telefonie geeignet ist. Dank der HD-Sprachqualität macht niemand aus, ob Sie den Anruf mit einem Telefon, Ihrem Handy oder der Smartwatch entgegennehmen.</p>
+			</section>
+
+			<section class="inner topic-item">
+				<img src="img/image-02.png" alt="Flexible Kanäle für jeden Anspruch." title="Flexible Kanäle für jeden Anspruch.">
+
+				<h2>Flexible Kanäle für jeden Anspruch.</h2>
+
+				<p>Sie können gerade nicht rangehen? Seien Sie dennoch erreichbar, indem Sie einfach Ihren Anwesenheitsstatus anpassen und währenddessen via Chat kontaktiert werden. Zusätzlich steht Ihnen eine Vielzahl anderer Kommunikationskanäle zur Verfügung wie Online-Meeting, Videotelefonie oder Bildschirmfreigabe. Sie wählen, welcher davon für die entsprechende Situation am besten geeignet ist.</p>
+			</section>
+
+			<section class="inner topic-item">
+				<img src="img/image-03.png" alt="Sichere Datenverwaltung in der Cloud." title="Sichere Datenverwaltung in der Cloud.">
+
+				<h2>Sichere Datenverwaltung in der Cloud.</h2>
+
+				<p>Neben einwandfreier Kommunikation ist eine übersichtliche Datenverwaltung essenziell für effizientes Arbeiten. Dafür sorgt die Cloud, auf der Sie all Ihre wichtigen Dokumente abspeichern und wieder abrufen – sogar offline. So haben Sie auch ausserhalb Ihres Arbeitsplatzes immer die aktuellste Version zur Hand.</p>
+			</section>
+		</section>
+
+		<section class="section">
+			<section class="inner topic-item">
+				<div class="topic-name"><span>Checklist</span></div>
+
+				<img src="img/image-04.png" alt="10 Fragen zur Vorbereitung" title="10 Fragen zur Vorbereitung">
+				
+				<span class="title-topic">10 Fragen zur Vorbereitung</span>
+				<h1>Wie IP-fit ist Ihr Unternehmen?</h1>
+
+				<p>Die Bereitstellung der IP-Infrastruktur ist eine grosse Aufgabe für unsere Netzanbieter und mit hohen Investments verbunden. Auch auf Seiten der Unternehmen kann es Aufwände bei der Umstellung geben, vor allem wenn ältere Telefonanlagen überdacht oder erneuert werden müssen. Umso wichtiger ist eine gute Vorbereitung.</p>
+			</section>
+
+			<section class="inner consulting">
+				<h2>Diese 10 Fragen stellt Ihnen Ihr kompetenter IT-Partner und unterstützt Sie bei der Lösungsfindung dazu:</h2>
+
+				<ol>
+					<li><p>Ist Ihre bestehende Telefonanlage IP-tauglich?</p></li>
+					<li><p>Wie gross ist das finanzielle und zeitliche Sparpotenzial, wenn Sie die Telefonanlage auf die IP-taugliche Cloud auslagern?</p></li>
+					<li><p>Welche Vorteile bringt eine Kombination von IP-Telefonie und Internetzugang?</p></li>
+					<li><p>Welche Konsequenzen hat ein Ausfall der Telefonie für Sie?</p></li>
+					<li><p>Brauchen Sie Spezialanwendungen, wie zum Beispiel Lifttelefone oder Alarmanlagen?</p></li>
+					<li><p>Sind genügend interne und externe Nummern vorhanden?</p></li>
+					<li><p>Sind alle Telefone bereits drahtlos und mit Display ausgestattet, z.B. für die Anzeige der Rufnummer?</p></li>
+					<li><p>Kann Ihr Team heute schon unterwegs von der Festnetznummer aus anrufen?</p></li>
+					<li><p>Besteht Bedarf an zusätzlichen Kommunikationsmöglichkeiten wie Chat, Bildschirmfreigabe oder Online-Konferenzen?</p></li>
+					<li><p>Passt das aktuelle Tarifmodell zur effektiven Nutzung? Wo gibt es zusätzliches Sparpotenzial?</p></li>
+				</ol>
+
+				<p class="bolder">Lassen Sie sich persönlich beraten bei der IP-Umstellung – es lohnt sich.</p>
+			</section>
+		</section>
+
+		<!-- Scripts -->
+		<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+		<script src="js/vendor/jquery.validate.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
